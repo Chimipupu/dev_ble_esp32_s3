41a010ef7c239c3b5eb13ac742ba0f90a553647a
Chimipupu <chiminyanyan@gmail.com>
2026-05-09 04:49:14 +0900

    BLEの基本動作の周期的なスリープ&起床を実装

--- dev_ble_esp32_s3.ino
+++ dev_ble_esp32_s3.ino
@@ -27,11 +27,14 @@
 #include "app_ble.h"
 
 // -----------------------------------------------------------
+// DeepSleep周期
+#define DEEPSLEEP_PERIOD_MS 1000000 // 1秒周期でDeepSleep
 // 基板のGPIO
 #define RGBLED_PIN        48   // YD-ESP32-S3 RGBLED @GPIO 48
 
-static app_ble_cfg_t s_ble_cfg;
+RTC_DATA_ATTR uint32_t g_boot_cnt = 0;
 
+static app_ble_cfg_t s_ble_cfg;
 static void _gpio_init(void);
 #if 0
 static void _i2c_init(void);
@@ -39,6 +42,8 @@ static void _i2s_init(void);
 static void _spi_init(void);
 #endif
 static void _uart_init(void);
+static void _deepsleep_init(void);
+
 // -----------------------------------------------------------
 // [Static]
 static void _gpio_init(void)
@@ -51,9 +56,24 @@ static void _uart_init(void)
     Serial.begin(115200);
 }
 
+static void _deepsleep_init(void)
+{
+    // DeepSleepの周期に設定
+    esp_sleep_enable_timer_wakeup(DEEPSLEEP_PERIOD_MS * 1000); // ms -> us
+}
+
 // -----------------------------------------------------------
 void setup()
 {
+    // 起床要因がDeepSleep
+    if(esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER) {
+        g_boot_cnt++;
+    }
+    Serial.printf("Boot Count: %lu\n", g_boot_cnt);
+
+    // DeepSleep初期化
+    _deepsleep_init();
+
     // GPIO初期化
     _gpio_init();
 
@@ -62,13 +82,16 @@ void setup()
 
     // BLE初期化
     s_ble_cfg.is_use_long_range_mode = true; // BLE Long Rage Mode: 有効
-    s_ble_cfg.adv_interval_ms = 1000; // アドバタイズ周期: 1秒周期
-    s_ble_cfg.tx_power_dbm = 9; // 送信出力: 9dB
+    s_ble_cfg.adv_interval_ms = 1000;        // アドバタイズ周期: 1秒周期
+    s_ble_cfg.tx_power_dbm = 9;              // 送信出力: 9dB
     app_ble_init(&s_ble_cfg);
 }
 
 void loop()
 {
-    // BLEメイン
+    // BLEのメイン処理
     app_ble_main();
+
+   // DeepSleep開始
+    esp_deep_sleep_start();
 }
